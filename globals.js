a095e6b775ed271a71e5eabc0e7eaf6dc6dd67d3
Amaninreal <amaninreallife03@gmail.com>
2024-12-18 15:41:33 +0530

    Added readme file

--- globals.js
+++ globals.js
@@ -1,7 +1,6 @@
 import dotenv from 'dotenv';
 
-// Load .env variables
 dotenv.config();
 
-export const loginUrl = process.env.LOGIN_BASEURL; // Export the URL as a named export
-export const testTimeout = 90000; // Timeout value in milliseconds
+export const loginUrl = process.env.LOGIN_BASEURL;
+export const testTimeout = 90000;
